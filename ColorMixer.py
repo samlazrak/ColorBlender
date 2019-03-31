cb1463d0c88f84c4ed401cd0cd13eb9df1f8752f
Sam Lazrak <lazrak13@uab.edu>
2019-03-31 09:49:15 -0500

    pallet

--- ColorMixer.py
+++ ColorMixer.py
@@ -4,7 +4,7 @@ from colorutils import ArithmeticModel, Color
 
 @click.group()
 def main():
-    """
+  """
     Simple CLI to blend colors together.
 
     Usage:
@@ -12,27 +12,25 @@ def main():
     blend - blends two colors together when given the colors in rgb or hex
     python3 ColorMixer.py blend <r> <g> <b> <r> <g> <b>
     """
-    pass
+  pass
 
 
 @click.command()
-@click.option('--rgb', 'type', flag_value='rgb')
-@click.option('--hex', 'type', flag_value='hex')
-@click.argument('blend', nargs=-1)
+@click.option("--rgb", "type", flag_value="rgb")
+@click.option("--hex", "type", flag_value="hex")
+@click.argument("blend", nargs=-1)
 def blend(blend, type):
-    if type == 'rgb':
-        c1rgb = int(blend[0]), int(blend[1]), int(blend[2])
-        c2rgb = int(blend[3]), int(blend[4]), int(blend[5])
-        blended = Color(rgb=(c1rgb), arithmetic=ArithmeticModel.BLEND) + \
-            Color(rgb=(c2rgb))
-        print(blended)
-    if type == 'hex':
-        c1 = Color(hex=blend[0])
-        c2 = Color(hex=blend[1])
-        blended = Color(rgb=(c1), arithmetic=ArithmeticModel.BLEND) + \
-            Color(rgb=(c2))
-        print(blended)
+  if type == "rgb":
+    c1rgb = int(blend[0]), int(blend[1]), int(blend[2])
+    c2rgb = int(blend[3]), int(blend[4]), int(blend[5])
+    blended = Color(rgb=(c1rgb), arithmetic=ArithmeticModel.BLEND) + Color(rgb=(c2rgb))
+    print(blended)
+  if type == "hex":
+    c1 = Color(hex=blend[0])
+    c2 = Color(hex=blend[1])
+    blended = Color(rgb=(c1), arithmetic=ArithmeticModel.BLEND) + Color(rgb=(c2))
+    print(blended)
 
 
 if __name__ == "__main__":
-    blend()
+  blend()
